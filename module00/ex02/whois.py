2cc81f7ed0b6062cf1bf59bdadadd3ae5c7cc41f
kcisse <kcisse@f1br7s14.paris.42.school>
2026-07-13 14:26:07 +0200

    In this exercice I Make a program that takes a number as argument, checks whether it is odd, even or zero, and prints the result

---
+++ module00/ex02/whois.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def odd_even_zero(num):
+    if num == 0:
+        print("I'm Zero.")
+    elif num % 2:
+        print("I'm Odd.")
+    else:
+        print("I'm Even.")
+
+def main():
+    if(len(sys.argv) < 2):
+        return
+    try:
+        assert len(sys.argv) == 2, "more than one argument is provided"
+        num = int(sys.argv[1])
+        odd_even_zero(num)
+    except AssertionError as e:
+        print(f"AssertionError {e}")
+    except Exception as ex:
+        print("AssertionError margument is not an integer")
+    
+
+if __name__=="__main__":
+    main()
