3e5d550531b6073b53c494bd8ff56b93ff43fb4e
kcisse <kcisse@f1br7s12.paris.42.school>
2026-07-15 18:28:05 +0200

    add ex02 of module01

---
+++ module01/ex02/vector.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Vector:
+
+    def __init__(self, values, shape=None):
+        self.values = np.array(values)
+        self.shape = shape
+    
+    # def .dot (self):
+
+    # def .T(self):
+
+    # def __mul__(self, other):
+
+    # def __truediv__(self, other):
+
