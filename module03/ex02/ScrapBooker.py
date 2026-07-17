6e97165db1337df8803ab21de0fa0e819b0bbcba
kcisse <kcisse@f1br6s12.paris.42.school>
2026-07-17 15:03:16 +0200

    add change

--- module03/ex02/ScrapBooker.py
+++ module03/ex02/ScrapBooker.py
@@ -1,5 +1,33 @@
+import numpy as np
+
 class ScrapBooker:
 	def crop(self, array, dim, position=(0,0)):
+		try:
+			s = array[dim[1]:dim[0] + 1, position[1]:position[0]]
+			return s
+		except (ValueError, TypeError) as e:
+			return None
+
 	def thin(self, array, n, axis):
-	def juxtapose(self, array, n, axis):
-	def mosaic(self, array, dim):
+		# try:
+			s = np.delete(array, list(range(n - 1, array.shape[1], n)), axis=n)
+			return s
+		# except (ValueError, TypeError) as e:
+		# 	return None
+
+	def juxtapose(self, array, n, a):
+		try:
+			s = array
+			for i in range(n - 1):
+				s = np.append(s, array, axis = a)
+			return s
+		except (ValueError, TypeError) as e:
+			return None
+
+	# def mosaic(self, array, dim):
+
+
+spb = ScrapBooker()
+arr2 = np.array("A B C D E F G H I".split() * 6).reshape(-1,9)
+arr2 = np.array("A B C D E F G H I".split() * 6).reshape(-1,9)
+print(spb.thin(arr2,3,0))
